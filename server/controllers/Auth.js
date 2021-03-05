a9d8449f3c8df02675d331014b7965e3dd143187
Tudor Iovita <iovitatudor@gmail.com>
2021-03-05 19:53:11 +0200

    fix bug

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -4,16 +4,21 @@ class Auth {
     async login(req, res)
     {
         // const user = await User.findOne({login: req.body.login, password: req.body.password})
-        const user = await User.findOne()
+        let user = await User.findOne()
 
-        const userT = await User.find()
-
-        console.log(userT);
-        
         if (user) {
             // req.session.user = user
             return res.status(200).json({user, message: 'ok'})
         }else{
+            user = await new User({
+                name: 'Tudor',
+                email: 'email@email.com',
+                phone: '04902394023',
+                login: 'manager10',
+                password: 'passw',
+                type: 'employee',
+                active: false,
+            }).save()
             return res.status(200).json({user: false, message: 'error'})
         }
     }
