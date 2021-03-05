94256d1c547389cac83bbcfe8782398256f2c161
Tudor Iovita <iovitatudor@gmail.com>
2021-03-05 19:49:06 +0200

    fix bug

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -4,8 +4,12 @@ class Auth {
     async login(req, res)
     {
         // const user = await User.findOne({login: req.body.login, password: req.body.password})
-        const user = await User.findOne({login: req.body.login})
+        const user = await User.findOne()
 
+        const userT = await User.find()
+
+        console.log(userT);
+        
         if (user) {
             // req.session.user = user
             return res.status(200).json({user, message: 'ok'})
